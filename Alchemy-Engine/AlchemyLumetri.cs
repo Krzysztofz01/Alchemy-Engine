c020741118eb45149b6996e1c9850aa4bd243eb3
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2020-03-16 15:20:24 +0100

    Brightness and Contrast filter

--- Alchemy-Engine/AlchemyLumetri.cs
+++ Alchemy-Engine/AlchemyLumetri.cs
@@ -69,5 +69,73 @@ namespace Alchemy_Engine
             }
             this.processingBackup.Add(bmp);
         }
+
+        //Values from -100 - 100
+        public void brightnessFilter(int value)
+        {
+            Bitmap bmp = getItem();
+            unsafe
+            {
+                BitmapData bitmapData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, bmp.PixelFormat);
+                int bytesPerPixel = Bitmap.GetPixelFormatSize(bmp.PixelFormat) / 8;
+                int heightInPixel = bitmapData.Height;
+                int widthInBytes = bitmapData.Width * bytesPerPixel;
+                byte* ptrFirstPixel = (byte*)bitmapData.Scan0;
+
+                Parallel.For(0, heightInPixel, y => {
+                    byte* currentLine = ptrFirstPixel + (y * bitmapData.Stride);
+                    for (int x = 0; x < widthInBytes; x += bytesPerPixel)
+                    {
+                        for(int px=0; px<3; px++)
+                        {
+                            int nVal = currentLine[x + px] + value;
+                            if (nVal < 0) nVal = 0;
+                            if (nVal > 255) nVal = 255;
+                            currentLine[x + px] = (byte)nVal;
+                        }
+                    }
+                });
+                bmp.UnlockBits(bitmapData);
+            }
+            this.processingBackup.Add(bmp);
+        }
+
+        //Values from -100 - 100
+        public void contrastFilter(int value)
+        {
+            if (value < -100) value = -100;
+            if (value > 100) value = 100;
+            double pixel = 0;
+            double contrast = Math.Pow((100.0 + value) / 100.0, 2); 
+            Bitmap bmp = getItem();
+            unsafe
+            {
+                BitmapData bitmapData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, bmp.PixelFormat);
+                int bytesPerPixel = Bitmap.GetPixelFormatSize(bmp.PixelFormat) / 8;
+                int heightInPixel = bitmapData.Height;
+                int widthInBytes = bitmapData.Width * bytesPerPixel;
+                byte* ptrFirstPixel = (byte*)bitmapData.Scan0;
+
+                Parallel.For(0, heightInPixel, y => {
+                    byte* currentLine = ptrFirstPixel + (y * bitmapData.Stride);
+                    for (int x = 0; x < widthInBytes; x += bytesPerPixel)
+                    {
+                        for (int px = 0; px < 3; px++)
+                        {
+                            pixel = currentLine[x + px] / 255.0;
+                            pixel -= 0.5;
+                            pixel *= contrast;
+                            pixel += 0.5;
+                            pixel *= 255;
+                            if (pixel < 0) pixel = 0;
+                            if (pixel > 255) pixel = 255;
+                            currentLine[x + px] = (byte)pixel;
+                        }
+                    }
+                });
+                bmp.UnlockBits(bitmapData);
+            }
+            this.processingBackup.Add(bmp);
+        }
     }
 }

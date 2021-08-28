a6871832f0fbaf3a931253c469ad731d670af168
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-08-28 02:26:16 +0200

    Color comparer and addition pallete implementation

--- AlchemyEngine/AlchemyEngine/Extensions/BitmapExtension.cs
+++ AlchemyEngine/AlchemyEngine/Extensions/BitmapExtension.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using AlchemyEngine.Processing;
+using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AlchemyEngine.Extensions
 {
@@ -14,5 +21,105 @@ namespace AlchemyEngine.Extensions
             int height = (bitmap.Height * percent) / 100;
             return new Bitmap(bitmap, new Size(width, height));
         }
+
+        public static IEnumerable<Color> GetPallete(this Bitmap bitmap, PalleteGenerator palleteGenerator)
+        {
+            switch (palleteGenerator)
+            {
+                case PalleteGenerator.AdditionMethod: return GeneratePalleteAdditionMethod(bitmap);
+                case PalleteGenerator.CubeMethod: return GeneratePalleteCubeMethod(bitmap);
+                default: throw new ArgumentOutOfRangeException("Invalid PalleteGenerator argument.");
+            }
+        }
+
+        public static async Task<IEnumerable<Color>> GetPalleteAsync(this Bitmap bitmap, PalleteGenerator palleteGenerator)
+        {
+            switch (palleteGenerator)
+            {
+                case PalleteGenerator.AdditionMethod: return await Task.Run(() => GeneratePalleteAdditionMethod(bitmap));
+                case PalleteGenerator.CubeMethod: return await Task.Run(() => GeneratePalleteCubeMethod(bitmap));
+                default: throw new ArgumentOutOfRangeException("Invalid PalleteGenerator argument.");
+            }
+        }
+
+        public static async Task<IEnumerable<Color>> GetPalleteAsync(this Bitmap bitmap, PalleteGenerator palleteGenerator, CancellationToken cancellationToken)
+        {
+            switch (palleteGenerator)
+            {
+                case PalleteGenerator.AdditionMethod: return await Task.Run(() => GeneratePalleteAdditionMethod(bitmap), cancellationToken);
+                case PalleteGenerator.CubeMethod: return await Task.Run(() => GeneratePalleteCubeMethod(bitmap), cancellationToken);
+                default: throw new ArgumentOutOfRangeException("Invalid PalleteGenerator argument.");
+            }
+        }
+
+        private static IEnumerable<Color> GeneratePalleteAdditionMethod(Bitmap bitmap)
+        {
+            //Data preparation step
+            var scaledBitmap = bitmap.Scale(50);
+            var colors = new Dictionary<Color, int>();
+
+            BitmapData bitmapData = scaledBitmap
+                .LockBits(new Rectangle(0, 0, scaledBitmap.Width, scaledBitmap.Height), ImageLockMode.ReadOnly, scaledBitmap.PixelFormat);
+
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(scaledBitmap.PixelFormat) / 8;
+            int byteCount = bitmapData.Stride * scaledBitmap.Height;
+            byte[] pixels = new byte[byteCount];
+            IntPtr firstPixelPtr = bitmapData.Scan0;
+
+            Marshal.Copy(firstPixelPtr, pixels, 0, pixels.Length);
+            int heightInPixels = bitmapData.Height;
+            int widthInPixels = bitmapData.Width;
+
+            //Scaning step
+            for(int y=0; y < heightInPixels; y++)
+            {
+                int currentLine = y * bitmapData.Stride;
+                
+                for(int x=0; x < widthInPixels; x++)
+                {
+                    //Pixel color access
+                    var currentPixel = Color.FromArgb(pixels[currentLine + x + 2], pixels[currentLine + x + 1], pixels[currentLine + x]);
+
+                    if (colors.ContainsKey(currentPixel))
+                    {
+                        colors[currentPixel]++;
+                    }
+                    else
+                    {
+                        colors.Add(currentPixel, 1);
+                    }
+                }
+            }
+
+            scaledBitmap.UnlockBits(bitmapData);
+
+            //Sorting step
+            colors.OrderByDescending(x => x.Value);
+
+            //Filtering step
+            var filteredPallete = new List<Color>();
+
+            foreach(var color in colors)
+            {
+                if (!filteredPallete.Any()) filteredPallete.Add(color.Key);
+
+                if (ColorComparer.Distance(color.Key, filteredPallete.Last()) > 45) filteredPallete.Add(color.Key);
+
+                if (filteredPallete.Count == 10) break;
+            }
+
+            return filteredPallete;
+        }
+
+        private static IEnumerable<Color> GeneratePalleteCubeMethod(Bitmap bitmap)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public enum PalleteGenerator
+    {
+        CubeMethod,
+        AdditionMethod
     }
 }
---
+++ AlchemyEngine/AlchemyEngine/Processing/ColorComparer.cs
@@ -0,0 +1,28 @@
+﻿using AlchemyEngine.Structures;
+using System;
+using System.Drawing;
+
+namespace AlchemyEngine.Processing
+{
+    public static class ColorComparer
+    {
+        public static float Distance(Color a, Color b)
+        {
+            int redDistance = Math.Abs(a.R - b.R);
+            int greenDistance = Math.Abs(a.G - b.G);
+            int blueDistance = Math.Abs(a.B - b.B);
+
+            return redDistance + greenDistance + blueDistance;
+        }
+
+        public static float Distance(Cmyk a, Cmyk b)
+        {
+            float cyanDistance = Math.Abs(a.Cyan - b.Cyan);
+            float magentaDistance = Math.Abs(a.Magenta - b.Magenta);
+            float yelloDistance = Math.Abs(a.Yellow - b.Yellow);
+            float keyDistance = Math.Abs(a.Key - b.Key);
+
+            return cyanDistance + magentaDistance + yelloDistance + keyDistance;
+        }
+    }
+}

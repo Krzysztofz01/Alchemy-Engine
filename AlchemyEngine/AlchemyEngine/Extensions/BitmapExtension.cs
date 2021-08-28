2f5a04d61c2bea5e1054126042c7b2e0c87a8c43
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2021-08-28 23:49:18 +0200

    Cube pallete implementation

--- AlchemyEngine/AlchemyEngine/Extensions/BitmapExtension.cs
+++ AlchemyEngine/AlchemyEngine/Extensions/BitmapExtension.cs
@@ -113,7 +113,90 @@ namespace AlchemyEngine.Extensions
 
         private static IEnumerable<Color> GeneratePalleteCubeMethod(Bitmap bitmap)
         {
-            throw new NotImplementedException();
+            //Local index definition function
+            static int getIndex(int value)
+            {
+                if (value < 85) return 0;
+                if (value > 170) return 2;
+                return 1;
+            }
+
+            //Data preparation step
+            var scaledBitmap = bitmap.Scale(50);
+            var colorMatrix = new List<Color>[3, 3, 3];
+
+            for (int xAxis = 0; xAxis < 3; xAxis++)
+            {
+                for (int yAxis = 0; yAxis < 3; yAxis++)
+                {
+                    for (int zAxis = 0; zAxis < 3; zAxis++)
+                    {
+                        colorMatrix[xAxis, yAxis, zAxis] = new List<Color>();
+                    }
+                }
+            }
+
+            BitmapData bitmapData = scaledBitmap
+                .LockBits(new Rectangle(0, 0, scaledBitmap.Width, scaledBitmap.Height), ImageLockMode.ReadOnly, scaledBitmap.PixelFormat);
+
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(scaledBitmap.PixelFormat) / 8;
+            int byteCount = bitmapData.Stride * scaledBitmap.Height;
+            byte[] pixels = new byte[byteCount];
+            IntPtr firstPixelPtr = bitmapData.Scan0;
+
+            Marshal.Copy(firstPixelPtr, pixels, 0, pixels.Length);
+            int heightInPixels = bitmapData.Height;
+            int widthInPixels = bitmapData.Width;
+
+            //Scaning step
+            for (int y = 0; y < heightInPixels; y++)
+            {
+                int currentLine = y * bitmapData.Stride;
+
+                for (int x = 0; x < widthInPixels; x++)
+                {
+                    //Pixel color access
+                    var currentPixel = Color.FromArgb(pixels[currentLine + x + 2], pixels[currentLine + x + 1], pixels[currentLine + x]);
+
+                    int redIndex = getIndex(currentPixel.R);
+                    int greenIndex = getIndex(currentPixel.G);
+                    int blueIndex = getIndex(currentPixel.B);
+
+                    colorMatrix[redIndex, greenIndex, blueIndex].Add(currentPixel);
+                }
+            }
+
+            scaledBitmap.UnlockBits(bitmapData);
+
+            //Merge step
+            var colors = new List<Color>();
+
+            for (int r = 0; r < 3; r++)
+            {
+                for (int g = 0; g < 3; g++)
+                {
+                    for (int b = 0; b < 3; b++)
+                    {
+                        int rSum = 0;
+                        int gSum = 0;
+                        int bSum = 0;
+                        int colorCount = colorMatrix[r, g, b].Count;
+
+                        if (colorCount == 0) continue;
+
+                        foreach(var color in colorMatrix[r, g, b])
+                        {
+                            rSum += color.R;
+                            gSum += color.G;
+                            bSum += color.B;
+                        }
+
+                        colors.Add(Color.FromArgb(rSum / colorCount, gSum / colorCount, bSum / colorCount));
+                    }
+                }
+            }
+
+            return colors;
         }
     }
 
